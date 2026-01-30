b2a3c63a99ce3ec615ff602632b5d0cb35178f54
vaishnavi jaiswal <vaishnavijaiswal707@gmail.com>
2026-01-30 13:05:45 +0530

    register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 function App() {
-  return <Dashboard />;
+  // return <Dashboard />;
+  return <Register />;
 }
 
 export default App;
